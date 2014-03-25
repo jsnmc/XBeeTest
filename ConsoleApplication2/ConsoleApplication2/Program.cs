9bb650d4f1896c0a4cef9ba54f288fea36b1edaa
jsnmc <jsnmcdev@gmail.com>
2014-03-25 01:50:18 -0400

    Initial checkin

---
+++ ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO.Ports;
+using System.Threading;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            // initialize the sensor port, mine was registered as COM8, you may check yours
+            // through the hardware devices from control panel
+            SerialPort sensor = new SerialPort("COM4", 9600, Parity.None, 8, StopBits.One);
+            int bytesToRead = 0;
+            string message;
+            sensor.Open();
+            try
+            {
+                while (true)
+                {
+                    // check if there are bytes incoming
+                    bytesToRead = sensor.BytesToRead;
+                    if (bytesToRead > 0)
+                    {
+                        byte[] input = new byte[bytesToRead];
+                        // read the Xbee's input
+                        sensor.Read(input, 0, bytesToRead);
+                        // convert the bytes into string
+                        message = System.Text.Encoding.UTF8.GetString(input);
+                        Console.WriteLine(message);
+                    }
+                }
+
+            }
+            finally
+            {
+                // again always close the serial ports!
+                sensor.Close();
+            }
+            
+        }
+
+        
+
+    }
+}
---
+++ NetduinoApplication1/NetduinoApplication1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading;
+using Microsoft.SPOT;
+using Microsoft.SPOT.Hardware;
+using SecretLabs.NETMF.Hardware;
+using SecretLabs.NETMF.Hardware.Netduino;
+using System.IO.Ports;
+
+namespace NetduinoApplication1
+{
+    public class Program
+    {
+
+        private static string message = @"LED"; 
+
+        public static void Main()
+        {
+            // Instantiate the communications
+            // port with some basic settings
+            SerialPort port = new SerialPort(
+              "COM1", 9600, Parity.None, 8, StopBits.One);
+
+            // Open the port for communications
+            port.Open();
+            OutputPort ledPort = new OutputPort(Pins.ONBOARD_LED, false);
+            byte[] buffer = new byte[message.Length];
+            buffer = System.Text.Encoding.UTF8.GetBytes(message);
+            try
+            {
+                while (true)
+                {
+                    ledPort.Write(true);
+                    Thread.Sleep(200);
+                    port.Write(buffer, 0, buffer.Length);
+                    ledPort.Write(false);
+                    Thread.Sleep(5000);
+                }
+
+            }
+            finally
+            {
+                port.Close();
+            }
+        }
+
+    }
+}
